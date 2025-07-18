5675a556b4373477b4f936230d736857e264148f
AeinkaraverlVithusana <vithusana.a22@gmail.com>
2025-07-18 09:29:16 +0530

    Update sidebar.tsx

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -13,6 +13,7 @@ import {
 import { TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Button } from "@/components/ui/button"
 import { X } from "lucide-react"
+import Logo from "@/assets/Logo.png"
 
 interface SidebarProps {
   isSidebarOpen: boolean
@@ -21,16 +22,11 @@ interface SidebarProps {
 
 const Sidebar = ({ isSidebarOpen, toggleSidebar }: SidebarProps) => {
   return (
-    <aside className={`w-64 bg-white border-r border-gray-200 flex flex-col transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static absolute inset-y-0 left-0 z-50`}>
+    <aside className={`w-64 bg-white border-r border-gray-200 flex flex-col transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static fixed top-0 left-0 h-screen z-50`}>
       {/* Header */}
       <div className="p-6 border-b border-gray-100 flex justify-between items-center">
         <div className="flex items-center gap-3">
-          <div className="w-8 h-8 bg-green-600 rounded-full flex items-center justify-center">
-            <div className="w-4 h-4 bg-white rounded-full"></div>
-          </div>
-          <div>
-            <h1 className="text-lg font-semibold text-gray-900">JuristQuest</h1>
-          </div>
+          <img src={Logo}></img>
         </div>
         <Button variant="ghost" size="icon" onClick={toggleSidebar} className="lg:hidden">
           <X className="h-4 w-4" />
